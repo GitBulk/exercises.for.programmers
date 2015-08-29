ab42276ca2256a704c9d1692e880faa0b98f036f
Shaun <shaun.finglas@googlemail.com>
2015-08-29 12:00:48 +0100

    Excercise 8 complete.

---
+++ dotnet/Excercises.For.Programmers/Excercises.For.Programmers/Excercise8/Input.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Excercises.For.Programmers.Excercise8
+{
+    static class Input
+    {
+        public static int Parse(string message)
+        {
+            while (true)
+            {
+                Console.Write(message);
+
+                try
+                {
+                    return Int32.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                }
+            }
+        }
+    }
+}
--- dotnet/Excercises.For.Programmers/Excercises.For.Programmers/Excercise8/PizzaParty.cs
+++ dotnet/Excercises.For.Programmers/Excercises.For.Programmers/Excercise8/PizzaParty.cs
@@ -4,8 +4,6 @@ using NUnit.Framework;
 
 namespace Excercises.For.Programmers.Excercise8
 {
-    // Todo: Handle pluralization.
-
     public class ConsolePizzaSplitter
     {
         public void Execute()
@@ -21,25 +19,62 @@ namespace Excercises.For.Programmers.Excercise8
             Console.WriteLine("Each person gets {0} pieces of pizza.", pizzaSlices.SlicesPerPerson);
             Console.WriteLine("There are {0} leftover pieces.", pizzaSlices.LeftOverSlices);
         }
+    }
 
-        static class Input
+    public class PizzaSplitter
+    {
+        public SplitPizza Split(int numberOfPizzas, int numberOfPeople)
         {
-            public static int Parse(string message)
-            {
-                while (true)
-                {
-                    Console.Write(message);
+            Debug.Assert(numberOfPeople > 1);
+            Debug.Assert(numberOfPizzas > 1);
 
-                    try
-                    {
-                        return int.Parse(Console.ReadLine());
-                    }
-                    catch (FormatException)
-                    {
-                    }
-                }
+            int splitPizza = SplitPizza(numberOfPizzas);
+            int slicesPerPerson = SlicesPerPerson(numberOfPeople, splitPizza);
+            int leftOvers = LeftOvers(numberOfPeople, splitPizza);
+
+            while (IsNotEvenlyDivided(slicesPerPerson))
+            {
+                splitPizza += SplitPizza(numberOfPizzas);
+                slicesPerPerson = SlicesPerPerson(numberOfPeople, splitPizza);
+                leftOvers = LeftOvers(numberOfPeople, splitPizza);
             }
+
+            return new SplitPizza(slicesPerPerson, leftOvers);
         }
+
+        private static int LeftOvers(int numberOfPeople, int splitPizza)
+        {
+            return splitPizza % numberOfPeople;
+        }
+
+        private static int SlicesPerPerson(int numberOfPeople, int splitPizza)
+        {
+            return splitPizza / numberOfPeople;
+        }
+
+        private static int SplitPizza(int numberOfPizzas)
+        {
+            const int splitFactor = 2;
+            return numberOfPizzas * splitFactor;
+        }
+
+        private static bool IsNotEvenlyDivided(int slicesPerPerson)
+        {
+            return slicesPerPerson % 2 != 0 || slicesPerPerson == 0;
+        }
+    }
+
+    public class SplitPizza
+    {
+        public SplitPizza(int slicesPerPerson, int leftOvers)
+        {
+            SlicesPerPerson = slicesPerPerson;
+            LeftOverSlices = leftOvers;
+        }
+
+        public int SlicesPerPerson { get; set; }
+        
+        public int LeftOverSlices { get; set; }
     }
 
     [TestFixture]
@@ -99,70 +134,5 @@ namespace Excercises.For.Programmers.Excercise8
             Assert.That(split.SlicesPerPerson, Is.EqualTo(2));
             Assert.That(split.LeftOverSlices, Is.EqualTo(2));
         }
-    }
-
-    public class PizzaSplitter
-    {
-        public SplitPizza Split(int numberOfPizzas, int numberOfPeople)
-        {
-            Debug.Assert(numberOfPeople > 1);
-            Debug.Assert(numberOfPizzas > 1);
-
-            var halfedPizzas = numberOfPizzas * 2;
-            var slicesPerPerson = halfedPizzas / numberOfPeople;
-
-            if (slicesPerPerson % 2 != 0 || slicesPerPerson == 0)
-            {
-                halfedPizzas += numberOfPizzas * 2;
-                slicesPerPerson = halfedPizzas / numberOfPeople;
-
-                if (slicesPerPerson % 2 != 0 || slicesPerPerson == 0)
-                {
-                    halfedPizzas += numberOfPizzas * 2;
-                    slicesPerPerson = halfedPizzas / numberOfPeople;
-
-                    if (slicesPerPerson % 2 != 0 || slicesPerPerson == 0)
-                    {
-                        halfedPizzas += numberOfPizzas * 2;
-                        slicesPerPerson = halfedPizzas / numberOfPeople;
-
-                        if (slicesPerPerson % 2 != 0 || slicesPerPerson == 0)
-                        {
-                            halfedPizzas += numberOfPizzas * 2;
-                            slicesPerPerson = halfedPizzas / numberOfPeople;
-
-                            if (slicesPerPerson % 2 != 0 || slicesPerPerson == 0)
-                            {
-                                halfedPizzas += numberOfPizzas * 2;
-                                slicesPerPerson = halfedPizzas / numberOfPeople;
-
-                            }
-                        }
-                    }
-                }
-
-
-            }
-
-            var leftOvers = halfedPizzas % numberOfPeople;
-
-            return new SplitPizza()
-                   {
-                       SlicesPerPerson = slicesPerPerson,
-                       LeftOverSlices = leftOvers
-                   };
-        }
-
-        public bool IsEven(int slicesOfPizza)
-        {
-            return slicesOfPizza % 2 == 0;
-        }
-    }
-
-    public class SplitPizza
-    {
-        public int SlicesPerPerson { get; set; }
-        
-        public int LeftOverSlices { get; set; }
     }
 }
